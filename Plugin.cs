6716b636bb05c8a83cd39635ad31ee1c1f5b1a35
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2023-08-22 20:08:21 -0400

    setup 2

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
 
 namespace CustomItemAPI
 {
     public class Plugin
     {
+        private const string Author = "KPG Dev Team";
 
+        private const string Name = "Custom Items API";
+
+        private const string Description = "Allows for other plugins to create custom items. ";
+
+        public const string Version = "0.0.1";
+
+        [PluginPriority(LoadPriority.Lowest)]
+        [PluginEntryPoint(Name, Version, Description, Author)]
+        public void Init()
+        {
+            Log.Info("Custom Items API Loaded! ");
+        }
     }
 }
