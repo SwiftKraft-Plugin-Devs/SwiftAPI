815b943b0eba869fc19c0ac670b0f9f176d7088f
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2024-01-20 16:29:17 -0500

    a

---
+++ API/CustomItems/CustomItemConsumableEffect.cs
@@ -0,0 +1,19 @@
+﻿using CustomPlayerEffects;
+using InventorySystem.Items;
+using PluginAPI.Core;
+
+namespace SwiftAPI.API.CustomItems
+{
+    public class CustomItemConsumableEffect<T> : CustomItemConsumable where T : StatusEffectBase
+    {
+        public float Duration;
+        public bool AddDuration;
+
+        public override void EndConsume(Player _player, ItemBase _item)
+        {
+            _player.EffectsManager.EnableEffect<T>(Duration, AddDuration);
+
+            base.EndConsume(_player, _item);
+        }
+    }
+}
