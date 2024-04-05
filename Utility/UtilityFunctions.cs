edc76b890582980948a1ba0412ce4c05232071d2
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2024-04-05 16:45:49 -0400

    a

--- Utility/UtilityFunctions.cs
+++ Utility/UtilityFunctions.cs
@@ -1,5 +1,11 @@
-﻿using PluginAPI.Core;
+﻿using Footprinting;
+using InventorySystem.Items;
+using InventorySystem.Items.Pickups;
+using InventorySystem.Items.ThrowableProjectiles;
+using Mirror;
+using PluginAPI.Core;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace SwiftAPI.Utility
 {
@@ -11,5 +17,21 @@ namespace SwiftAPI.Utility
             players.RemoveAll((p) => EventHandler.JoiningPlayers.Contains(p));
             return players;
         }
+
+        public static void SpawnActive(this ThrowableItem item, Vector3 position, float fuseTime = -1f, Player owner = null)
+        {
+            if (item.Projectile is not TimeGrenade)
+                return;
+
+            TimeGrenade grenade = (TimeGrenade)Object.Instantiate(item.Projectile, position, Quaternion.identity);
+            if (fuseTime >= 0)
+                grenade._fuseTime = fuseTime;
+            grenade.NetworkInfo = new PickupSyncInfo(item.ItemTypeId, item.Weight, item.ItemSerial);
+            grenade.PreviousOwner = new Footprint(owner != null ? owner.ReferenceHub : ReferenceHub.HostHub);
+            NetworkServer.Spawn(grenade.gameObject);
+            grenade.ServerActivate();
+        }
+
+        public static ThrowableItem CreateThrowable(this ItemType type, Player player = null) => (player != null ? player.ReferenceHub : ReferenceHub.HostHub).inventory.CreateItemInstance(new ItemIdentifier(type, ItemSerialGenerator.GenerateNext()), false) as ThrowableItem;
     }
 }
