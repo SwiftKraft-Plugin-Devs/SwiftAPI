885d3eea98d4ba24176136e0e091743dd31e24e0
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2024-06-30 17:06:34 -0400

    a

---
+++ API/CustomRoles/CustomRoleBase.cs
@@ -0,0 +1,11 @@
+﻿using PlayerRoles;
+
+namespace SwiftAPI.API.CustomRoles
+{
+    public abstract class CustomRoleBase(RoleTypeId role, params ItemType[] loadout)
+    {
+        public RoleTypeId Role = role;
+
+        public ItemType[] Loadout = loadout;
+    }
+}
---
+++ API/CustomRoles/CustomRoleManager.cs
@@ -0,0 +1,16 @@
+﻿using PluginAPI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftAPI.API.CustomRoles
+{
+    public static class CustomRoleManager
+    {
+        //public static readonly Dictionary<Player, >
+
+
+    }
+}
